4e7463db0fa3258a3d9e99f60c8bfd646bb0c2a8
TehStud <igortadic.msc@gmail.com>
2022-08-30 20:01:45 +0200

    platform collision detection

--- index.js
+++ index.js
@@ -88,6 +88,14 @@ function animate() {
   } else {
     player.velocity.x = 0
   }
+
+  if (player.position.y + player.height <= platform.position.y &&
+    player.position.y + player.height + player.velocity.y >= platform.position.y &&
+    player.position.x + player.width >= platform.position.x &&
+    player.position.x <= platform.position.x + platform.width) {
+    player.velocity.y = 0;
+  }
+
 }
 
 animate()
